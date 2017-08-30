9320a924fa35d0dc472558b0f5c1a3b218562c97
hkailee <leehongkai@gmail.com>
2017-08-30 17:53:46 +0800

    update

--- Python/FileDirectoryTools/walkingDirectoryTree.py
+++ Python/FileDirectoryTools/walkingDirectoryTree.py
@@ -8,5 +8,17 @@ def lister(root):
         for fname in fileshere:
             print(os.path.join(dirname, fname))     # handle one file
 
+def listerFilter(root):
+    matches = []
+    for (dirname, subshere, fileshere) in os.walk(root):
+        for fname in fileshere:
+            if fname.endswith('.py'):
+                pathname = os.path.join(dirname, fname)
+                if 'import' in open(pathname).read():
+                    matches.append(pathname)
+    for name in matches:
+        print(name)
+
 if __name__ == '__main__':
     lister(sys.argv[1])
+    listerFilter(sys.argv[1])
