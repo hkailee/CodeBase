89f6f35634615c95e23fe6e722211d4b2eadd20f
hkailee <leehongkai@gmail.com>
2017-08-30 10:53:20 +0800

    update

---
+++ Python/teststreams.py
@@ -0,0 +1,17 @@
+#!/Users/leehongkai/virtualenv_python1/bin/python3
+"""read number till eof and show squares"""
+
+def interact():
+    print('Hello stream word')
+    while True:
+        try:
+            reply = input('Enter a number>')
+        except EOFError:
+            break
+        else:
+            num = int(reply)
+            print('%d squared is %d' % (num, num ** 2))
+    print('Bye')
+
+if __name__ == '__main__':
+    interact()
