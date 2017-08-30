310ff69472662dc51ae6cc43ecf5d873994d7155
hkailee <leehongkai@gmail.com>
2017-08-30 16:36:05 +0800

    update

--- Python/FileDirectoryTools/commands.py
+++ Python/FileDirectoryTools/commands.py
@@ -1,12 +1,13 @@
 #!/usr/local/bin/python3
 
 from sys import argv
-from scanfile import scanner
+from scanfile import scanner2
 
 class UnknownCommand(Exception):
     pass
 
-def processLine(line):                  # define a function
+######### Method One ########
+def processLine1(line):                  # define a function
     if line[0] == '*':                  # applied to each line
         print("Ms.", line[1:-1])
     elif line[0] == '+':
@@ -14,8 +15,20 @@ def processLine(line):                  # define a function
     else:
         raise UnknownCommand(line)
 
+
+######## Method Two ########
+commands = {'*': 'Ms.', '+': 'Mr.'}     # expanding the definition
+def processLine2(line):
+    try:
+        print(commands[line[0]], line[1:-1])
+    except KeyError:
+        raise UnknownCommand(line)
+
 if __name__ == '__main__':
     filename = 'data.txt'
     if len(argv) == 2:
         filename = argv[1]                  # allow filename cmd arg
-    scanner(filename, processLine)          # start the scanner
+    print("Method One")
+    scanner2(filename, processLine1)          # start the scanner with Method One
+    print("Method Two")
+    scanner2(filename, processLine2)          # start the scanner with Method Two
--- Python/FileDirectoryTools/scanfile.py
+++ Python/FileDirectoryTools/scanfile.py
@@ -1,7 +1,14 @@
-def scanner (name, function):
+########## First scanner ##########
+def scanner1 (name, function):
     file = open(name, 'r')              # create a file object
     while True:
         line = file.readline()          # call file method
         if not line: break              # until end-of-file
         function(line)                  # call a function object
     file.close()
+
+########## Second scanner ##########
+# Recommended for higher speed using for loop than the above while loop
+def scanner2(name, function):
+    for line in open(name, 'r'):        # scan line-by-line
+        function(line)                  # call a function
