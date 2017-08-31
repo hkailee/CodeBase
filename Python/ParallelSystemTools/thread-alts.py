7c95cf2e5bd7f5a03e2d4e128f30c43d95fe6ef0
hkailee <leehongkai@gmail.com>
2017-08-31 12:13:46 +0800

    update

---
+++ Python/ParallelSystemTools/thread-alts.py
@@ -0,0 +1,19 @@
+#!/usr/local/bin/python3
+
+import _thread
+
+def action(i):                                      # function run in threads
+    print(i ** 32)
+
+class Power:
+    def __init__(self, i):
+        self.i = i
+    def action(self):                               # bound method run in threads
+        print(self.i ** 32)
+
+_thread.start_new_thread(action, (2,))              # simple function
+
+_thread.start_new_thread((lambda: action(2)), ())   # lambda function to defer
+
+obj = Power(2)
+_thread.start_new_thread(obj.action, ())            # bound method object
