95736a6c1ca9827a2a4066f1eb0ed6459ad4f642
hkailee <leehongkai@gmail.com>
2017-09-02 15:31:52 +0800

    update

---
+++ Python/ParallelSystemTools/thread-add-synch.py
@@ -0,0 +1,29 @@
+#!/usr/local/bin/python3
+
+"""
+print 200 each teime, because shared resource access synchronized
+"""
+
+import threading, time
+
+count = 0
+
+def adder(addlock):             # shared lock object passed in
+    global count
+    with addlock:               # auto acquire/release around stmt
+        count = count + 1       # update a shared name in global scope
+    time.sleep(0.5)
+    with addlock:               # auto acquire/release around stmt
+        count = count + 1           # only 1 thread updating each time
+
+addlock = threading.Lock()
+threads = []
+for i in range(100):
+    thread = threading.Thread(target=adder, args=(addlock,))
+    thread.start()
+    threads.append(thread)
+
+for thread in threads:
+    thread.join()
+
+print(count)
