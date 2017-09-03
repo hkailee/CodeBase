3e236c9a02d5009c7f37f5de84acc2673ab0b525
hkailee <leehongkai@gmail.com>
2017-09-03 11:32:24 +0800

    update

--- Python/ParallelSystemTools/testexit_os.py
+++ Python/ParallelSystemTools/testexit_os.py
@@ -1,7 +1,11 @@
 #!/usr/local/bin/python3
 
 """
-typically used within a forked child process on Unix
+typically used within a forked child process on Unix;
+Unlike sys.exit, os._exit is immune to both try/except and try/finally interception,
+i.e. the calliing process will exits immediately instead of raising and exception
+that could be trapped and ignored. In fact, the process also exit without flushing
+output stream buffers or running cleanup handlers.
 """
 
 def outahere():
