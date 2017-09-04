54f7edc14992b0017cb616fb2e92489e8778b594
hkailee <leehongkai@gmail.com>
2017-09-04 21:45:12 +0800

    update

---
+++ Python/ParallelSystemTools/socket_preview.py
@@ -0,0 +1,9 @@
+#!/usr/local/bin/python3
+
+"""
+sockets for cross-task communication: start threads to communicate over sockets;
+independent programs can too, because sockets are system-wide, much like fifos;
+see the GUI and Internet parts of the book for more realistic socket use cases;
+some socket servers
+"""
+
