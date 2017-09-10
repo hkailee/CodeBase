a2d7f5d047da1b0376806111f0311ebddeef6db0
hkailee <leehongkai@gmail.com>
2017-09-10 13:11:25 +0800

    update

--- Python/Databases/shelve_Eg.py
+++ Python/Databases/shelve_Eg.py
@@ -1,8 +1,8 @@
 #!/usr/local/bin/python3
 
 """
-Not much of a new topic compared to dbm and pickle earlier, simply a combination
-of the DBM files and object pickling, but with several advantage for big dataset
+Not much of a new topic compared to dbm and pickle earlier, simply using
+DBM and object pickling internally, but with several advantages to retrive big dataset
 (i.e. Analogy of a shelf storing multiple DBM files indexed by keys for retrieval):
 1. To store an in-memory object by key, the shelve module first serializes the
 object to a string with the pickle module, and then it stores that string in a
@@ -12,7 +12,7 @@ serialized string by key from a DBM file with the dbm module, and then converts
 it back to the original in-memory object with the pickle module.
 """
 
-import pickle
+import shelve
 
 # Create new database
 table = {'a': [1, 2, 3],
