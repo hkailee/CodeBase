7deb7a2469f4fc1a7642fe839b32478a49131d97
hkailee <leehongkai@gmail.com>
2017-09-10 23:40:53 +0800

    update

--- Python/Databases/makedictsFromSQL.py
+++ Python/Databases/makedictsFromSQL.py
@@ -1,135 +1,22 @@
 #!/usr/local/bin/python3
 
 """
-the connect call’s arguments are usually the only thing that can vary across
-different database systems. For example, in the MySQL interface this call accepts
-a network host’s domain name, user name, and password, passed as keyword
-arguments instead, and the Oracle example sketched earlier expects a more specific
-string syntax. Once we’ve gotten past this platform- specific call, though,
-the rest of the API is largely database neutral.
+convert list of row tuples to list of row dicts with field name keys
+this is not a command-line utility: hardcoded self-test if run
 """
 
 import sqlite3
 
-# Create new database (Kind of Boilerplate code)
-conn = sqlite3.connect('dbase_Sqlite')
+def makedicts(cursor, query, params=()):
+    cursor.execute(query, params)
+    colnames = [desc[0] for desc in cursor.description]
+    rowdicts = [dict(zip(colnames, row)) for row in cursor.fetchall()]
+    return rowdicts
 
-curs = conn.cursor()
-tblcmd = 'create table people (name char(30), job char(10), pay int(4))'
-curs.execute(tblcmd)
-
-# To insert data
-print(sqlite3.paramstyle)  # this returns "qmark" tells this module accepts ? for replacement targets
-
-curs.execute('insert into people values (?, ?, ?)', ('Bob', 'dev', 5000))
-print(curs.rowcount)
-
-curs.executemany('insert into people values (?, ?, ?)',
-                 [ ('Sue', 'mus', '70000'),
-                   ('Ann', 'mus', '60000')])
-
-print(curs.rowcount)
-
-rows = [['Tom', 'mgr', 100000],
-        ['Kim', 'adm', 30000],
-        ['pat', 'dev', 90000]]
-
-
-for row in rows:
-    curs.execute('insert into people values (? , ?, ?)', row)
-
-# Without a commit, a connection rollback or close call, as well as the
-# connection’s __del__ deletion method, will back out uncommitted changes.
-conn.commit()
-
-# Different styles to  view data
-curs.execute('select * from people')
-for row in curs.fetchall():
-    print(row)
-
-curs.execute('select * from people')
-for (name, job, pay) in curs.fetchall():
-    print(name, ':', pay)
-
-curs.execute('select name from people')
-names = curs.fetchall()
-print(names)
-
-curs.execute('select * from people')
-names = [rec[0] for rec in curs.fetchall()]
-print(names)
-
-curs.execute('select * from people')
-while True:
-    row = curs.fetchone()
-    if not row:
-        break
-    print(row)
-
-curs.execute('select * from people')
-while True:
-    rows = curs.fetchmany()  # size=N optional argument
-    if not row:
-        break
-    for row in rows:
-        print(row)
-
-curs.execute('select name, job from people where pay > 60000')
-print(curs.fetchall())
-
-query = 'select name, job from people where pay >= ? order by name'
-curs.execute(query, [60000])
-for row in curs.fetchall():
-    print(row)
-
-# Update a record
-curs.execute('update people set pay=? where pay <= ?', [65000, 60000])
-print(curs.rowcount)
-curs.execute('select * from people')
-print(curs.fetchall())
-
-# To delete a record
-curs.execute('delete from people where name = ?', ['Bob'])
-curs.execute('delete from people where pay >= ?',(90000,))
-curs.execute('select * from people')
-print(curs.fetchall())
-
-conn.commit()
-
-# Using table/schema descriptions
-# The 7 values in tuple indicate (name, type_code, display_size, internal_size, precision, scale, null_ok)
-curs.execute('select * from people')
-print(curs.description)
-colnames = [desc[0] for desc in curs.description]
-print(colnames)
-
-for row in curs.fetchall():
-    for name, value in zip(colnames, row):
-        print(name, '\t=>', value)
-    print()
-
-# Record dictionaries construction
-curs.execute('select * from people')
-colnames = [desc[0] for desc in curs.description]
-rowdicts = []
-for row in curs.fetchall():
-    newdict = {}
-    for name, val in zip(colnames, row):
-        newdict[name] = val
-    rowdicts.append(newdict)
-
-for row in rowdicts:
-    print(row)
-
-# More powerful record dictionaries construction in python
-curs.execute('select * from people')
-colnames = [desc[0] for desc in curs.description]
-rowdicts = []
-for row in curs.fetchall():
-    rowdicts.append(dict(zip(colnames, row)))
-print(rowdicts[0])
-
-curs.execute('select * from people')
-colnames = [desc[0] for desc in curs.description]
-rowdicts = [dict(zip(colnames, row)) for row in curs.fetchall()]
-print(rowdicts[0])
+if __name__ == '__main__': # self test
+    conn = sqlite3.connect('dbase_Sqlite')
+    cursor = conn.cursor()
+    query = 'select name, pay from people where pay < ?'
+    lowpay = makedicts(cursor, query, [70000])
+    for rec in lowpay:
+        print(rec)
