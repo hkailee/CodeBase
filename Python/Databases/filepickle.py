73be22ee1f0267e230a73be47e35fd04da020fca
hkailee <leehongkai@gmail.com>
2017-09-10 11:28:50 +0800

    Removing all files in .gitignore

--- Python/Databases/filepickle.py
+++ Python/Databases/filepickle.py
@@ -9,17 +9,37 @@ import pickle
 def saveDbase(filename, object):
     'save object to file'
     file = open(filename, 'wb')
-    pickle.dump(object, file)
-    file.close()
+    pickle.dump(object, file)           # pickle to binary file
+    file.close()                        # any file-like object will do
 
 def loadDbase(filename):
     'load object from file'
     file = open(filename, 'rb')
-    object = pickle.load(file)
-    file.close()
+    object = pickle.load(file)          # unpickle from binary file
+    file.close()                        # re-creates object in memory
     return object
 
+# Create myfile database
 L = [0]
 D = {'x':0, 'y':L}
-table = {'A': L, 'B': D}
-saveDbase('myfile', table)
+table = {'A': L, 'B': D}                # L appears twice
+saveDbase('myfile', table)              # serialize to file
+
+# Loading existing myfile database created
+table = loadDbase('myfile')
+print(table)
+
+# The above are just lists, tuples, and dictionaries. How about class instances?
+class Rec:
+    def __init__(self, hours):
+        self.hours = hours
+    def pay(self, rate=50):
+        return self.hours * rate
+
+bob = Rec(40)
+pickle.dump(bob, open('bobrec', 'wb'))
+
+# Load the bobrec database created above
+rec = pickle.load(open('bobrec', 'rb'))
+print(rec.hours)
+print(rec.pay())
